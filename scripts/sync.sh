ddf5e90658d475f35b04a6fa1aae7e545440622d
dev-queiroz <queirozdouglas466@gmail.com>
2025-05-24 15:14:08 -0300

    Update sync script to create app directory and clone repository

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+mkdir -p /app
 # shellcheck disable=SC2164
-cd /app || mkdir /app && cd /app
+cd /app
 
 if [ -d ".git" ]; then
   git pull origin main
 else
-  git clone https://github.com/dev-queiroz/linux-vnc-github.git /app
+  git clone https://github.com/SEU_USUARIO/SEU_REPO.git /app
 fi
