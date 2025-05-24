f694bcc00fa1988ce1176f31c32426b76b661d5d
dev-queiroz <queirozdouglas466@gmail.com>
2025-05-24 15:11:36 -0300

    Configure VNC startup script and initialize VNC server

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+export USER=root
+
 # Sincroniza com GitHub
 ./sync.sh
 
-# Configura VNC
+# Configura o arquivo xstartup do VNC para iniciar XFCE
+mkdir -p /root/.vnc
+echo '#!/bin/bash
+xrdb $HOME/.Xresources
+startxfce4 &' > /root/.vnc/xstartup
+chmod +x /root/.vnc/xstartup
+
+# Inicia o VNC Server
 export DISPLAY=:1
 vncserver :1 -geometry 1280x800 -depth 24
 
-# Inicia noVNC
-websockify --web=/usr/share/novnc/ --wrap-mode=ignore 6080 localhost:5901
+# Inicia o noVNC
+websockify --web=/usr/share/novnc 6080 localhost:5901
