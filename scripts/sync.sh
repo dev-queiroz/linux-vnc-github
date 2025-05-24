91ea3e5d85cf308f3c0c43d00b0b2dc22c046d4c
dev-queiroz <queirozdouglas466@gmail.com>
2025-05-24 15:21:01 -0300

    Update sync script to clone the correct repository for VNC setup

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -7,5 +7,5 @@ cd /app
 if [ -d ".git" ]; then
   git pull origin main
 else
-  git clone https://github.com/SEU_USUARIO/SEU_REPO.git /app
+  git clone https://github.com/dev-queiroz/linux-vnc-github.git /app
 fi
